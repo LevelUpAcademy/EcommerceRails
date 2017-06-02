02f15766a1a48c850b353409e8d325fbf020e8e2
Jonatan Kapps <jonatankapps@Kapps.local>
2017-06-01 21:23:23 -0300

    Added product forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  resources :products
+
   get 'home/index'
   root 'home#index'
+
+  scope :admin do
+    get 'products/new'
+    post 'products/create'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
